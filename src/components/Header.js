ef5ba2595dc4bb67c3f3ac0349f0b4c52a362b9f
bz <bilalzaafar@gmail.com>
2022-04-12 16:37:42 +0500

    UI updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Clicked");
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Hello" onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Indecision'
-}
+  title: "Task Tracker",
+};
 
-export default Header
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
